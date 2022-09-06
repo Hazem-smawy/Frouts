25bf614fb690f5c12eec920cd172b6a1cc9a8abf
hazem-smawy <hazemsmawy@gmail.com>
2022-09-06 17:24:27 +0300

    Initial Commit

---
+++ Frouts/FroutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FroutsApp.swift
+//  Frouts
+//
+//  Created by hazem smawy on 9/6/22.
+//
+
+import SwiftUI
+
+@main
+struct FroutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
